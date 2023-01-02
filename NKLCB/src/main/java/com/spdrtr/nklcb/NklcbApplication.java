fc885673f757fb5c8c4b9148b265c3ab37f5c812
100gammmit <100minha@naver.com>
2023-01-02 22:46:00 +0900

    Update NklcbApplication.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/NklcbApplication.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/NklcbApplication.java
@@ -1,12 +1,7 @@
 package com.spdrtr.nklcb;
 
-import com.spdrtr.nklcb.repository.ArticleRepository;
-import com.spdrtr.nklcb.service.Crawling;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class NklcbApplication {
