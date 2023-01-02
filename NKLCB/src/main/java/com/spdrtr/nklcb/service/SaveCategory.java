b2952fd4459b1c09cc12986569a916e94f9e85c6
100gammmit <100minha@naver.com>
2023-01-02 22:46:07 +0900

    Update SaveCategory.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/service/SaveCategory.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/service/SaveCategory.java
@@ -25,25 +25,29 @@ public class SaveCategory {
     public void saveCategory() throws InterruptedException {
         String url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.popularity_order&years=-1&locations=all";
         process(url);
+        logIn();
         String category_depth1;
         List<String> categories_depth2 = new ArrayList<>();
+        Thread.sleep(5000);
         try {
-            findButton("JobGroup_JobGroup__H1m1m").click();
-            final int Big_category_count = findButtons("JobGroupItem_JobGroupItem__xXzAi").size();
+            findElement("JobGroup_JobGroup__H1m1m").click();
+            final int Big_category_count = findElements("JobGroupItem_JobGroupItem__xXzAi").size();
+            List<WebElement> Big_categories = findElements("JobGroupItem_JobGroupItem__xXzAi");
             for(int i=0; i< Big_category_count; i++){
-                WebElement Big_category = findButtons("JobGroupItem_JobGroupItem__xXzAi").get(i);
+                WebElement Big_category = Big_categories.get(i);
                 category_depth1 = Big_category.getText();
                 System.out.println("상위카테고리:" + category_depth1);
                 Big_category.click();
 
-                findButton("JobCategory_JobCategory__btn__k3EFe").click();
+                findElement("JobCategory_JobCategory__btn__k3EFe").click();
                 categories_depth2 = getData("JobCategoryItem_JobCategoryItem__oUaZr");
                 for(String category_depth2 : categories_depth2){
                     Category category = Category.of(category_depth1, category_depth2);
                     categoryRepository.save(category);
                     System.out.println(category_depth2);
                 }
-                findButton("JobGroup_JobGroup__H1m1m").click();
+
+                findElement("JobGroup_JobGroup__H1m1m").click();
                 System.out.println("-------------------------------");
             }
         } catch (Exception o) {
