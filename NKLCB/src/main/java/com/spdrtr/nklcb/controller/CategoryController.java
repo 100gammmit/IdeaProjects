ad552cfc1e5fc045fe89607eac18442cada9f3f6
100gammmit <100minha@naver.com>
2023-01-02 22:45:57 +0900

    Update CategoryController.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/controller/CategoryController.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/controller/CategoryController.java
@@ -1,11 +1,9 @@
 package com.spdrtr.nklcb.controller;
 
-import com.spdrtr.nklcb.service.Crawling;
 import com.spdrtr.nklcb.service.SaveCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CategoryController {
