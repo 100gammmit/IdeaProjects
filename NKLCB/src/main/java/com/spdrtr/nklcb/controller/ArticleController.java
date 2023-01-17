fefdb8713d59737cda6e6645f2e970fded94d37d
100gammmit <100minha@naver.com>
2023-01-17 23:55:54 +0900

    Update ArticleController.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/controller/ArticleController.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/controller/ArticleController.java
@@ -1,23 +1,34 @@
 package com.spdrtr.nklcb.controller;
 
+import com.spdrtr.nklcb.domain.Article;
 import com.spdrtr.nklcb.service.ArticleService;
-import com.spdrtr.nklcb.service.SaveArticle;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @RestController
 public class ArticleController {
-    private final SaveArticle saveArticle;
     private final ArticleService articleService;
 
-    @GetMapping("/")
+    @GetMapping("/crawl")
     public String CrawlAndSave() throws InterruptedException {
-        saveArticle.saveArticle();
+        articleService.crawlArticle();
         return "index";
     }
 
+    @GetMapping("/")
+    public String FindArticle(Model model) {
+        Article article = articleService.findArticleById(1L);
+        model.addAttribute("title", article.getTitle());
+        model.addAttribute("enterprise", article.getEnterprise());
+        model.addAttribute("locate", article.getLocate());
+        model.addAttribute("reward", article.getReward());
+        return "";
+    }
 }
