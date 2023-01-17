96e2f80f4addd46deb8bc855a0eec5aed284da0a
100gammmit <100minha@naver.com>
2023-01-17 23:00:18 +0900

    Update ArticleRepository.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.spdrtr.nklcb.repository;
 
 import com.spdrtr.nklcb.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
