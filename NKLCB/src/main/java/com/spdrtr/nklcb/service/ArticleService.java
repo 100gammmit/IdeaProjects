439e50f0e4a42da9986ccb44124b76573f3b7bff
100gammmit <100minha@naver.com>
2023-01-17 23:00:09 +0900

    Update ArticleService.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/service/ArticleService.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/service/ArticleService.java
@@ -1,7 +1,64 @@
 package com.spdrtr.nklcb.service;
 
+import com.spdrtr.nklcb.domain.Article;
+import com.spdrtr.nklcb.dto.ArticleDto;
+import com.spdrtr.nklcb.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static com.spdrtr.nklcb.service.Crawling.getData;
+import static com.spdrtr.nklcb.service.Crawling.process;
 
 @Service
+@Transactional
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+    @Autowired
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public void crawlArticle() throws InterruptedException {
+        String url = "https://www.wanted.co.kr/wdlist?country=kr&job_sort=job.popularity_order&years=-1&locations=all";
+        process(url);
+        List<String> title = new ArrayList<>();
+        List<String> enterprise = new ArrayList<>();
+        List<String> locate = new ArrayList<>();
+        List<String> reward = new ArrayList<>();
+        try {
+            title = getData("job-card-position");
+            enterprise = getData("job-card-company-name");
+            locate = getData("job-card-company-location");
+            reward = getData("reward");
+        } catch (Exception o) {
+        }
+//        driver.quit();
+
+        for (int i = 0; i < title.size(); i++) {
+            System.out.println(i + 1);
+            System.out.println("title:" + title.get(i));
+            System.out.println("enterprise:" + enterprise.get(i));
+            System.out.println("locate:" + locate.get(i));
+            System.out.println("reward:" + reward.get(i) + "\n");
+            ArticleDto dto = ArticleDto.builder()
+                    .title(title.get(i))
+                    .enterprise(enterprise.get(i))
+                    .locate(locate.get(i))
+                    .reward(reward.get(i))
+                    .build();
+            articleRepository.save(dto.toEntity());
+        }
+    }
+
+    public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
+    }
+    public Article findArticleById(long ArticleId) {
+        return articleRepository.findById(ArticleId).get();
+    }
 }
