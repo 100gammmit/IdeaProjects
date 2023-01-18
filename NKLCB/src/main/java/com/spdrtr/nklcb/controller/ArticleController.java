458f826424942587ac0913ab0e7136491323dd23
100gammmit <100minha@naver.com>
2023-01-18 23:39:15 +0900

    Update ArticleController.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/controller/ArticleController.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/controller/ArticleController.java
@@ -8,12 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @RestController
 public class ArticleController {
+
     private final ArticleService articleService;
 
     @GetMapping("/crawl")
@@ -23,12 +24,31 @@ public class ArticleController {
     }
 
     @GetMapping("/")
-    public String FindArticle(Model model) {
+    public Map<String, Object> FindArticle() {
         Article article = articleService.findArticleById(1L);
-        model.addAttribute("title", article.getTitle());
-        model.addAttribute("enterprise", article.getEnterprise());
-        model.addAttribute("locate", article.getLocate());
-        model.addAttribute("reward", article.getReward());
-        return "";
+        Map<String, Object> articles = new HashMap<String, Object>();
+        articles.put("title", article.getTitle());
+        articles.put("enterprise", article.getEnterprise());
+        articles.put("locate", article.getLocate());
+        articles.put("reward", article.getReward());
+        return articles;
+    }
+
+    @GetMapping("/all")
+    public  List<Map<String, Object>> GetAllArticles() {
+        List<Article> allArticle = articleService.getAllArticles();
+        List<Map<String, Object>> allArticlesDB = new ArrayList<>();
+        for(int i=0; i < allArticle.size(); i++){
+            Map<String, Object> articleDB = new HashMap<>();
+
+            articleDB.put("title", allArticle.get(i).getTitle());
+            articleDB.put("enterprise", allArticle.get(i).getEnterprise());
+            articleDB.put("locate", allArticle.get(i).getLocate());
+            articleDB.put("reward", allArticle.get(i).getReward());
+
+            allArticlesDB.add(articleDB);
+        }
+
+        return allArticlesDB;
     }
 }
