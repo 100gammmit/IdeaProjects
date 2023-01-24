152f224bb487d18b71d0eacea4c94422ca2b5e58
100gammmit <100minha@naver.com>
2023-01-25 05:22:29 +0900

    Update CategoryDto.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/dto/CategoryDto.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/dto/CategoryDto.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
  * A DTO for the {@link com.spdrtr.nklcb.domain.Category} entity
  */
 public record CategoryDto(long id, String category_depth1, String category_depth2) implements Serializable {
-
     public static CategoryDto from(Category entity){
         return new CategoryDto(
                 entity.getId(),
