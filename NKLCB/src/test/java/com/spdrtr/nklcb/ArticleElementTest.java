3c2ff24c6f96a6d96a1a4f989c934eb307e6502e
100gammmit <100minha@naver.com>
2023-01-25 05:18:15 +0900

    Article 크롤링 관련 로직 테스트 작성
    article 크롤링 관련 로직 테스트를 작성하는 Test클래스 작성
    - image_url 추출 로직 테스트 추가

---
+++ NKLCB/src/test/java/com/spdrtr/nklcb/ArticleElementTest.java
@@ -0,0 +1,25 @@
+package com.spdrtr.nklcb;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArticleElementTest {
+    @Test
+    @DisplayName("채용보상금 String타입에서 int타입으로 추출")
+    void rewardStringToInt() {
+        int reward = Integer.parseInt("채용보상금 1,000,000원".replaceAll("[^0-9]", ""));
+
+        assertThat(reward).isEqualTo(1000000);
+    }
+
+    @Test
+    @DisplayName("이미지url 어트리뷰트에서 슬라이싱으로 url만 추출")
+    void getImageUrl() {
+        String iu = "background-image: url(\"https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fcompany%2F8567%2Fxzxxblm2ffsn1xdb__400_400.jpg&w=400&q=75\");";
+
+        assertThat(iu.substring(iu.indexOf("(")+2, iu.indexOf(")")-1)).isEqualTo("https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fcompany%2F8567%2Fxzxxblm2ffsn1xdb__400_400.jpg&w=400&q=75");
+    }
+
+}
