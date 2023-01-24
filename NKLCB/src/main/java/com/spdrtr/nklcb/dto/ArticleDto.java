b1e9c203769d20517d1d8ca3b82c59e34c50fc0d
100gammmit <100minha@naver.com>
2023-01-25 05:25:19 +0900

    reward int타입으로 변경 및 빌더에 image_url, official_url 추가

--- NKLCB/src/main/java/com/spdrtr/nklcb/dto/ArticleDto.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/dto/ArticleDto.java
@@ -9,11 +9,11 @@ import java.io.Serializable;
  * A DTO for the {@link com.spdrtr.nklcb.domain.Article} entity
  */
 public record ArticleDto(long id, String title, String condition, String enterprise, String deadline,
-                         String locate, String reward,
+                         String locate, int reward, String image_url,
                          String official_url) implements Serializable {
     @Builder
     public ArticleDto(long id, String title, String condition, String enterprise, String deadline,
-                      String locate, String reward,
+                      String locate, int reward, String image_url,
                       String official_url){
         this.id = id;
         this.title = title;
@@ -22,6 +22,7 @@ public record ArticleDto(long id, String title, String condition, String enterpr
         this.deadline = deadline;
         this.locate = locate;
         this.reward = reward;
+        this.image_url = image_url;
         this.official_url = official_url;
     }
 
@@ -31,6 +32,8 @@ public record ArticleDto(long id, String title, String condition, String enterpr
                 .enterprise(enterprise)
                 .locate(locate)
                 .reward(reward)
+                .image_url(image_url)
+                .official_url(official_url)
                 .build();
         return article;
     }
