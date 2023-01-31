9e5699261f4c0884373ee2ff187837c681d8c965
100gammmit <100minha@naver.com>
2023-02-01 01:56:12 +0900

    카테고리 매핑에 따른 DTO 수정
    카테고리 객체 멤버로 추가

--- NKLCB/src/main/java/com/spdrtr/nklcb/dto/ArticleDto.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/dto/ArticleDto.java
@@ -1,6 +1,7 @@
 package com.spdrtr.nklcb.dto;
 
 import com.spdrtr.nklcb.domain.Article;
+import com.spdrtr.nklcb.domain.Category;
 import lombok.Builder;
 
 import java.io.Serializable;
@@ -10,11 +11,11 @@ import java.io.Serializable;
  */
 public record ArticleDto(long id, String title, String condition, String enterprise, String deadline,
                          String locate, int reward, String image_url,
-                         String official_url) implements Serializable {
+                         String official_url, Category category) implements Serializable {
     @Builder
     public ArticleDto(long id, String title, String condition, String enterprise, String deadline,
                       String locate, int reward, String image_url,
-                      String official_url){
+                      String official_url, Category category){
         this.id = id;
         this.title = title;
         this.condition = condition;
@@ -24,6 +25,7 @@ public record ArticleDto(long id, String title, String condition, String enterpr
         this.reward = reward;
         this.image_url = image_url;
         this.official_url = official_url;
+        this.category = category;
     }
 
     public Article toEntity(){
@@ -34,6 +36,7 @@ public record ArticleDto(long id, String title, String condition, String enterpr
                 .reward(reward)
                 .image_url(image_url)
                 .official_url(official_url)
+                .category(category)
                 .build();
         return article;
     }
