aeb58dbe65cfc371c5bdcd9289b439e1b60ae3a2
100gammmit <100minha@naver.com>
2023-02-01 01:51:46 +0900

    Delete ArticleCategoryMappingRepository.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleCategoryMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spdrtr.nklcb.repository;
-
-import com.spdrtr.nklcb.domain.ArticleCategoryMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleCategoryMappingRepository extends JpaRepository<ArticleCategoryMapping, Long> {
-}
