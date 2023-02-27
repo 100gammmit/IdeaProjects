f25ab23c71b78f5c653f9e1207382777cc5cab05
100gammmit <100minha@naver.com>
2023-02-27 23:41:26 +0900

    검색을 위한 쿼리매서드 추가
    Containing으로 %LIKE% 쿼리를 돌려서 article을 검색하는 매서드 추가
    type은 우선 title과 enterprise만을 사용

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
@@ -3,8 +3,11 @@ package com.spdrtr.nklcb.repository;
 import com.spdrtr.nklcb.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByOriginalId(String original_id);
+    List<Article> findAllByTitleContaining(String keyword);
+    List<Article> findAllByEnterpriseContaining(String keyword);
 }
