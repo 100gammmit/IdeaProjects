82ccaacb2760cbb9dac843257b2ac2b031a2c0da
100gammmit <100minha@naver.com>
2023-03-08 23:54:10 +0900

    페이징 처리 쿼리 추가
    테스트 필요

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
@@ -1,6 +1,9 @@
 package com.spdrtr.nklcb.repository;
 
 import com.spdrtr.nklcb.domain.Article;
+import com.spdrtr.nklcb.domain.ArticleCategoryMapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +13,5 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByOriginalId(String original_id);
     List<Article> findAllByTitleContaining(String keyword);
     List<Article> findAllByEnterpriseContaining(String keyword);
+    Page<Article> findAllByArticleCategoryMappings(List<ArticleCategoryMapping> articleCategoryMappings, Pageable pageable);
 }
