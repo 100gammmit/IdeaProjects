a32d4922b5db069eadb89a0b125bd60ecc24b4c7
100gammmit <100minha@naver.com>
2023-03-20 23:58:05 +0900

    Update ArticleRepository.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
@@ -5,6 +5,7 @@ import com.spdrtr.nklcb.domain.ArticleCategoryMapping;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +14,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByOriginalId(String original_id);
     Page<Article> findAllByTitleContaining(String keyword, Pageable pageable);
     Page<Article> findAllByEnterpriseContaining(String keyword, Pageable pageable);
+
 //    Page<Article> findAllByArticleCategoryMappings(List<ArticleCategoryMapping> articleCategoryMappings, Pageable pageable);
 }
