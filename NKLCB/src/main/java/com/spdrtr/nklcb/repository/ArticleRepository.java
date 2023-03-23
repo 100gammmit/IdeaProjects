a203f8dfe673e0c314e05916a546931fe52099cf
100gammmit <100minha@naver.com>
2023-03-23 15:08:45 +0900

    메인 페이지에서 사용할 쿼리문 추가
    모든 article중 8개를 랜덤으로 추출하여 메인페이지에서 개시

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
@@ -1,11 +1,11 @@
 package com.spdrtr.nklcb.repository;
 
 import com.spdrtr.nklcb.domain.Article;
-import com.spdrtr.nklcb.domain.ArticleCategoryMapping;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +14,11 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByOriginalId(String original_id);
     Page<Article> findAllByTitleContaining(String keyword, Pageable pageable);
     Page<Article> findAllByEnterpriseContaining(String keyword, Pageable pageable);
+    @Query(value = "select * from Article order by RAND() limit 8", nativeQuery = true)
+    List<Article> findArticlesInMain();
 
-//    Page<Article> findAllByArticleCategoryMappings(List<ArticleCategoryMapping> articleCategoryMappings, Pageable pageable);
+    @Query("select ac from Article ac " +
+        "left outer join ArticleCategoryMapping acm on acm.article.id = ac.id " +
+            "where acm.category.id = :id")
+    Page<Article> findArticlesByCategoryId(@Param("id") Long id, Pageable pageable);
 }
