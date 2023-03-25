0362f3a57131f869136ac53b5a6c86ae36886da3
100gammmit <100minha@naver.com>
2023-03-26 00:05:47 +0900

    Update ArticleRepository.java

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/ArticleRepository.java
@@ -14,7 +14,7 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByOriginalId(String original_id);
     Page<Article> findAllByTitleContaining(String keyword, Pageable pageable);
     Page<Article> findAllByEnterpriseContaining(String keyword, Pageable pageable);
-    @Query(value = "select * from Article order by RAND() limit 8", nativeQuery = true)
+    @Query(value = "select * from Article order by RAND() limit 9", nativeQuery = true)
     List<Article> findArticlesInMain();
 
     @Query("select ac from Article ac " +
