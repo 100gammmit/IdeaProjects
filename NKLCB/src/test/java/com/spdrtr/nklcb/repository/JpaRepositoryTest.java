88d91d448eedb73379d92fe75e0b1fcd4962c030
100gammmit <100minha@naver.com>
2023-03-25 23:41:07 +0900

    테스트용 article, category 추가
    category1 - article 1개
    category2 - article 2개

--- NKLCB/src/test/java/com/spdrtr/nklcb/repository/JpaRepositoryTest.java
+++ NKLCB/src/test/java/com/spdrtr/nklcb/repository/JpaRepositoryTest.java
@@ -3,11 +3,17 @@ package com.spdrtr.nklcb.repository;
 import com.spdrtr.nklcb.domain.Article;
 import com.spdrtr.nklcb.domain.ArticleCategoryMapping;
 import com.spdrtr.nklcb.domain.Category;
-import com.spdrtr.nklcb.dto.ArticleDto;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +23,8 @@ class JpaRepositoryTest {
     private ArticleRepository articleRepository;
     private CategoryRepository categoryRepository;
     private ArticleCategoryMappingRepository articleCategoryMappingRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     public JpaRepositoryTest(
             @Autowired ArticleRepository articleRepository,
@@ -27,30 +35,49 @@ class JpaRepositoryTest {
         this.categoryRepository = categoryRepository;
     }
 
-    @Test
-    void save테스트(){
-        //given
-        long previousCount = articleRepository.count();
-        //when
-        ArticleDto articleDto = ArticleDto.builder()
-                .title("백엔드")
-                .enterprise("네이버")
-                .locate("경기.판교")
-                .originalId("12")
-                .reward("100")
-                .build();
-        articleRepository.save(articleDto.toEntity());
-
-        //then
-        assertThat(articleRepository.count()).isEqualTo(previousCount+1);
+    @AfterEach
+    public void idRestart() {
+        em.createNativeQuery("ALTER TABLE ARTICLE ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE ARTICLE_CATEGORY_MAPPING ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        em.createNativeQuery("ALTER TABLE CATEGORY ALTER COLUMN ID RESTART WITH 1").executeUpdate();
     }
 
     @Test
     @DisplayName("게시글과 카테고리가 정상적으로 매핑되어 DB에 저장되는지 확인")
     void ArticleCategoryMappingTest() {
         //given
-        long previousCount = articleCategoryMappingRepository.count();
+        insertData();
         //when
+
+        //then
+        assertThat(articleCategoryMappingRepository.findById(1L).get().getArticle()).isEqualTo(articleRepository.findById(1L).get());
+        assertThat(articleCategoryMappingRepository.findById(1L).get().getCategory()).isEqualTo(categoryRepository.findById(1L).get());
+        assertThat(articleCategoryMappingRepository.findById(2L).get().getArticle()).isEqualTo(articleRepository.findById(2L).get());
+        assertThat(articleCategoryMappingRepository.findById(2L).get().getCategory()).isEqualTo(categoryRepository.findById(2L).get());
+    }
+
+    @Test
+    @DisplayName("findbyOriginalId 테스트")
+    void findArticle() {
+        insertData();
+
+        assertThat(articleRepository.findByOriginalId("12").get().getTitle()).isEqualTo("백엔드");
+        assertThat(articleRepository.findByOriginalId("20")).isEmpty();
+    }
+
+    @Test
+    @DisplayName("findArticlesByCategoryId 테스트")
+    void findArticlesByCategoryId() {
+        // given
+        insertData();
+        // when
+        Pageable pageable = PageRequest.of(0, 5, Sort.by(Sort.Direction.ASC, "id"));
+        // then
+        assertThat(articleRepository.findArticlesByCategoryId(1L, pageable).get().findFirst().get()).isEqualTo(articleRepository.findById(1L).get());
+        assertThat(articleRepository.findArticlesByCategoryId(2L, pageable).getTotalElements()).isEqualTo(2);
+    }
+
+    void insertData() {
         Article article = Article.builder()
                 .title("백엔드")
                 .enterprise("네이버")
@@ -61,32 +88,37 @@ class JpaRepositoryTest {
         articleRepository.save(article);
         Category category = Category.of("개발", "소프트");
         categoryRepository.save(category);
-
         ArticleCategoryMapping articleCategoryMapping = new ArticleCategoryMapping();
         article.addMappingWithCategory(articleCategoryMapping);
         articleCategoryMapping.takeCategory(category);
         articleCategoryMappingRepository.save(articleCategoryMapping);
 
-        //then
-        assertThat(articleCategoryMappingRepository.count()).isEqualTo(previousCount+1);
-        assertThat(articleRepository.findById(1L).get().getArticleCategoryMappings()).isNotNull();
-        assertThat(categoryRepository.findById(1L).get().getArticleCategoryMappings()).isNotNull();
-    }
-
-    @Test
-    @DisplayName("findbyOriginalId 테스트")
-    void findArticle() {
-        ArticleDto dto = ArticleDto.builder()
-                .title("제목")
-                .enterprise("기업")
-                .originalId("12")
-                .locate("위치")
+        Article article1 = Article.builder()
+                .title("프론트")
+                .enterprise("카카오")
+                .locate("경기.판교")
+                .originalId("13")
                 .reward("100")
                 .build();
-        articleRepository.save(dto.toEntity());
+        articleRepository.save(article1);
+        Category category1 = Category.of("개발", "프론트");
+        categoryRepository.save(category1);
+        ArticleCategoryMapping articleCategoryMapping1 = new ArticleCategoryMapping();
+        article1.addMappingWithCategory(articleCategoryMapping1);
+        articleCategoryMapping1.takeCategory(category1);
+        articleCategoryMappingRepository.save(articleCategoryMapping1);
 
-        assertThat(articleRepository.findByOriginalId("12").get().getTitle()).isEqualTo("제목");
-        assertThat(articleRepository.findByOriginalId("13")).isEmpty();
+        Article article2 = Article.builder()
+                .title("데이터")
+                .enterprise("라인")
+                .locate("서울.강남")
+                .originalId("13")
+                .reward("100")
+                .build();
+        articleRepository.save(article2);
+        ArticleCategoryMapping articleCategoryMapping2 = new ArticleCategoryMapping();
+        article2.addMappingWithCategory(articleCategoryMapping2);
+        articleCategoryMapping2.takeCategory(category1);
+        articleCategoryMappingRepository.save(articleCategoryMapping2);
     }
-
 }
