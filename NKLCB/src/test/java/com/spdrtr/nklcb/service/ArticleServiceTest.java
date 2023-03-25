8eead38c016c075a66e21075d401a675d6665dd4
100gammmit <100minha@naver.com>
2023-03-25 23:41:14 +0900

    Update ArticleServiceTest.java

--- NKLCB/src/test/java/com/spdrtr/nklcb/service/ArticleServiceTest.java
+++ NKLCB/src/test/java/com/spdrtr/nklcb/service/ArticleServiceTest.java
@@ -1,16 +1,9 @@
 package com.spdrtr.nklcb.service;
 
-import com.spdrtr.nklcb.domain.Article;
 import com.spdrtr.nklcb.dto.ArticleDto;
 import com.spdrtr.nklcb.repository.ArticleRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -32,6 +25,7 @@ class ArticleServiceTest {
                 .enterprise("기업")
                 .locate("위치")
                 .reward("100")
+                .originalId("12")
                 .build();
         articleRepository.save(dto.toEntity());
 
