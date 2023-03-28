ab712789b5ac17cb148352842d712362450f0d98
100gammmit <100minha@naver.com>
2023-03-28 16:50:26 +0900

    Scheduling 어노테이션 추가
    게시글 업데이트 자동화를 위한 스프링 스케쥴링 기능 사용

--- NKLCB/src/main/java/com/spdrtr/nklcb/NklcbApplication.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/NklcbApplication.java
@@ -2,7 +2,9 @@ package com.spdrtr.nklcb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class NklcbApplication {
 
