c07e38cbe7e75a72d1102a938858df92e1d15d43
100gammmit <100minha@naver.com>
2023-03-28 18:45:13 +0900

    현재시간을 반환하는 매서드 작성
    스케줄링 테스트과정에서 매서드 실행시점 및 완료 시점을 확인하기 위한 현재 시간 표기용 매서드
    
    이후에 스케줄링에서 필요한 설정 매서드 생길 시 해당 클래스안에 작성할 예정

---
+++ NKLCB/src/main/java/com/spdrtr/nklcb/service/Scheduling.java
@@ -0,0 +1,20 @@
+package com.spdrtr.nklcb.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Scheduling {
+    /**
+     * 현재 시간을 "yyyy.MM.dd kk:mm:ss E요일" 형식으로 포멧하여 변환하는 매서드
+     * @return String(현재시간)
+     */
+    public static String getNowDateTime24() {
+        //long 타입으로 System.currentTimeMillis() 시스템에서 현재 시간 데이터를 받음
+        long time = System.currentTimeMillis();
+        //시간 데이터를 포멧하는 양식 설정
+        SimpleDateFormat dayTime = new SimpleDateFormat("yyyy.MM.dd kk:mm:ss E요일");
+        //위에서 설정한 양식대로 시간 데이터 String 타입으로 변환
+        String str = dayTime.format(new Date(time));
+        return str;
+    }
+}
