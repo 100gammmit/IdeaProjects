4b27cf364f42eb8e9e2b039c7b2ca9f2e60e9597
100gammmit <100minha@naver.com>
2023-04-11 13:44:07 +0900

    HomeController 작성
    root경로를 통해 접속하는 경우 메인페이지로 보내는 컨트롤러 작성

---
+++ NKLCB/src/main/java/com/spdrtr/nklcb/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.spdrtr.nklcb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("")
+    public String home() {
+        return "forward:/articles";
+    }
+}
