8f5e92abeed690c7f149edb016dbd19138f29076
100gammmit <100minha@naver.com>
2023-04-11 15:40:02 +0900

    position을 통해 category 도메인 객체를 찾는 쿼리 매서드 추가
    New article을 업데이트 할 때 카테고리를 매핑하기 위해서 해당 카테고리를 찾는 작업을 하는 용도의 매서드

--- NKLCB/src/main/java/com/spdrtr/nklcb/repository/CategoryRepository.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/repository/CategoryRepository.java
@@ -2,6 +2,12 @@ package com.spdrtr.nklcb.repository;
 
 import com.spdrtr.nklcb.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT c from Category c WHERE c.category_depth2 = :category")
+    Category findByCategoryPosition(@Param("category") String category);
 }
