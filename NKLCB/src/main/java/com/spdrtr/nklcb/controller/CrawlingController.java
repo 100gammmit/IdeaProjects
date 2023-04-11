e387a05c120532a371a918dfa41b909b01517824
100gammmit <100minha@naver.com>
2023-04-11 13:45:44 +0900

    크롤링 매서드 동작 시간대 표시
    크롤링 시작, 완료 시간대를 콘솔에 표시하는 코드 추가

--- NKLCB/src/main/java/com/spdrtr/nklcb/controller/CrawlingController.java
+++ NKLCB/src/main/java/com/spdrtr/nklcb/controller/CrawlingController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.spdrtr.nklcb.service.Scheduling.getNowDateTime24;
+
 @RequiredArgsConstructor
 @RestController
 public class CrawlingController {
@@ -12,7 +14,22 @@ public class CrawlingController {
 
     @GetMapping("/crawl")
     public String CrawlAndSave() throws InterruptedException {
+        System.out.println("\n");
+        System.out.println("=======================================");
+        System.out.println("[ArticleService] : [crawlAllArticleWithCategory]");
+        System.out.println("[Started] : " + getNowDateTime24());
+        System.out.println("=======================================");
+        System.out.println("\n");
+
         articleService.crawlAllArticleWithCategory();
+
+        System.out.println("\n");
+        System.out.println("=======================================");
+        System.out.println("[ArticleService] : [crawlAllArticleWithCategory]");
+        System.out.println("[Ended] : " + getNowDateTime24());
+        System.out.println("=======================================");
+        System.out.println("\n");
+
         return "index";
     }
 }
